b3e429e90b9405e90e57215e2c2ae03558af3fdb
Anthony T <ungegrieftfirma@gmail.com>
2023-07-12 02:24:13 +0200

    feature(waiting items): added Items for waiting state

--- src/main/java/com/laudynetwork/manhunt/Manhunt.java
+++ src/main/java/com/laudynetwork/manhunt/Manhunt.java
@@ -1,7 +1,9 @@
 package com.laudynetwork.manhunt;
 
 import com.laudynetwork.gameengine.game.backend.GameDataHandler;
-import com.laudynetwork.manhunt.game.WaitingListeners;
+import com.laudynetwork.manhunt.game.waiting.WaitingListeners;
+import com.laudynetwork.manhunt.game.waiting.items.WaitingItemHandler;
+import com.laudynetwork.networkutils.api.gui.GUIHandler;
 import com.laudynetwork.networkutils.api.messanger.backend.MessageCache;
 import lombok.Getter;
 import lombok.val;
@@ -14,6 +16,7 @@ public class Manhunt extends JavaPlugin {
     private static Manhunt INSTANCE;
     private ManhuntGame game;
     private MessageCache msgCache;
+    private GUIHandler<?> guiHandler;
 
     @Override
     public void onEnable() {
@@ -24,10 +27,16 @@ public class Manhunt extends JavaPlugin {
         this.msgCache.loadFileInCache(this.getResource("translations/plugins/de.json"), "de");
         this.msgCache.loadFileInCache(this.getResource("translations/plugins/en.json"), "en");
 
+        this.guiHandler = Bukkit.getServicesManager().getRegistration(GUIHandler.class).getProvider();
+
         val dataHandler = Bukkit.getServicesManager().getRegistration(GameDataHandler.class).getProvider();
         game = new ManhuntGame();
 
         val pm = Bukkit.getPluginManager();
+
+        val itemHandler = new WaitingItemHandler();
+        pm.registerEvents(itemHandler, this);
+
         pm.registerEvents(this.game, this);
         pm.registerEvents(new WaitingListeners(this.game), this);
         this.game.onLoad();
